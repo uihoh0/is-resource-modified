1abb33f332435c41c65f624c5b31c4285932466a
haofeng.zhao <haofeng.zhao@leyantech.com>
2019-05-16 16:49:58 +0800

    fix: cleartimeout处理出错

--- src/util.ts
+++ src/util.ts
@@ -4,16 +4,16 @@ export class Looper {
         this.intervalTime = intervalTime
     }
     private fn: Function
-    private timeout: any = 0
-    private intervalTime: number = 0
+    private intervalTime: any = 0
     run(): void {
-        const {fn, intervalTime} = this
-        this.timeout = setTimeout(() => {
+        const { fn, intervalTime } = this
+        clearTimeout(intervalTime)
+        this.intervalTime = setTimeout(() => {
             fn()
             this.run()
         }, intervalTime)
     }
     stop(): void {
-        clearTimeout(this.timeout)
+        clearTimeout(this.intervalTime)
     }
 }
